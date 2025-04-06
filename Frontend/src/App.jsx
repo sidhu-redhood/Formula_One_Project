ce1d6759efbfcaed5b5b350c2beb9a836f8a521f
Sidhu <sidhurockzz05@gmail.com>
2025-04-06 23:13:44 +0530

    removed backend and finished adding chakraUI

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Box>
+        <p fontSize="2xl" color="blue.500">
+            Hello, World!
+        </p>
+        <p fontSize="xl" color="green.500">
+            This is a Chakra UI example.
+        </p>
+    </Box>
+  )
+}
+
+export default App
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from "@/components/ui/provider";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider>
+      <App />
+    </Provider>
+  </StrictMode>
+);
